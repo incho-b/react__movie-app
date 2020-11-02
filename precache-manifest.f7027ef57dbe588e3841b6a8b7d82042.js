ca44b2f35fe9b8275c8ea4fae177bca29733059f
inchoB <8wycad.ic21@gmail.com>
2020-11-02 11:39:21 +0900

    Updates

---
+++ precache-manifest.f7027ef57dbe588e3841b6a8b7d82042.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60f46114965fd673a8c657db074299b",
+    "url": "/react__movie-app/index.html"
+  },
+  {
+    "revision": "49dafce4fe40d564a9fe",
+    "url": "/react__movie-app/static/css/main.6ab56ab4.chunk.css"
+  },
+  {
+    "revision": "28c6026cdb06ea54bfb2",
+    "url": "/react__movie-app/static/js/2.890d10fb.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/react__movie-app/static/js/2.890d10fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49dafce4fe40d564a9fe",
+    "url": "/react__movie-app/static/js/main.f825e648.chunk.js"
+  },
+  {
+    "revision": "e95a24883ce411b29720",
+    "url": "/react__movie-app/static/js/runtime-main.fe4551f5.js"
+  }
+]);
